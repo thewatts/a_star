0301ad3eef2ea9c27fa7aec0cf93516e3197c2ee
Nathaniel Watts <reg@nathanielwatts.com>
2014-01-28 22:39:33 -0700

    add G and F scores, and #coordinates method on Node

--- lib/node.rb
+++ lib/node.rb
@@ -25,9 +25,32 @@ class Node
     status == "walkable"
   end
 
+  def coordinates
+    [x, y]
+  end
+
+  def direction
+    if parent
+      return "diagonal"   if parent.x != x && parent.y != y
+      return "horizontal" if parent.y == y
+      return "vertical"   if parent.x == x
+    end
+  end
+
   def h
     x_diff = x - world.target.x
     y_diff = y - world.target.y
-    x_diff.abs + y_diff.abs
+    (x_diff.abs + y_diff.abs) * 10
+  end
+
+  def g
+    return 0 if character?
+    return directional_score + parent.g if parent
+    directional_score
+  end
+
+  def directional_score
+    return 14 if direction == "diagonal"
+    return 10 if direction == "horizontal" || direction == "vertical"
   end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -34,6 +34,32 @@ describe Node do
     expect(node.parent).to eq parent_node
   end
 
+  it "has coordinates" do
+    node = Node.new({x: 5, y: 4})
+    expect(node.coordinates).to eq [5, 4]
+  end
+
+  it "knows the diagonal direction from the parent" do
+    parent_node = Node.new({ x: 0, y: 0})
+    node        = Node.new({ x: 1, y: 1 })
+    node.parent = parent_node
+    expect(node.direction).to eq "diagonal"
+  end
+
+  it "knows the horizontal direction from the parent" do
+    parent_node = Node.new({ x: 0, y: 0})
+    node        = Node.new({ x: 1, y: 0})
+    node.parent = parent_node
+    expect(node.direction).to eq "horizontal"
+  end
+
+  it "knows the vertical direction from the parent" do
+    parent_node = Node.new({ x: 0, y: 0})
+    node        = Node.new({ x: 0, y: 1})
+    node.parent = parent_node
+    expect(node.direction).to eq "vertical"
+  end
+
   it "belongs to a world" do
     world = World.new
     node = Node.new({}, world)
@@ -50,7 +76,45 @@ describe Node do
       ]
       world = World.new(world_data)
       node = world.node_at(0,0)
-      expect(node.h).to eq 4
+      expect(node.h).to eq 40
+    end
+
+    it "has a G score (G cost from parent + direction cost)" do
+      parent_node = Node.new({ x: 0, y: 0, value: 'C'}) # starting node
+      node        = Node.new({ x: 0, y: 1})
+      node.parent = parent_node
+      expect(node.g).to eq 10
+    end
+
+    it "has a G score, 2 nodes from the character" do
+      character          = Node.new({ x: 0, y: 0, value: 'C'})
+      parent_node        = Node.new({ x: 1, y: 0 })
+      node               = Node.new({ x: 3, y: 1})
+      parent_node.parent = character
+      node.parent        = parent_node
+
+      expect(parent_node.g).to eq 10
+      expect(node.g).to eq 24
+    end
+
+    it "has an F score (distance from Char by path + distance from T)" do
+      world_data = [
+        "C   ",
+        "    ",
+        "   T"
+      ]
+      world               = World.new(world_data)
+      node                = world.node_at(2, 2)
+      parent_node         = world.node_at(2, 1)
+      parents_parent_node = world.node_at(1, 0)
+      character           = world.character
+
+      node.parent                = parent_node
+      parent_node.parent         = parents_parent_node
+      parents_parent_node.parent = character
+
+      expect(node.h).to eq 10
+      expect(node.g).to eq 34
     end
 
   end
